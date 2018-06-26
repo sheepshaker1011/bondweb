8ed1c949bfc322e6fa4a05f212e094b313f254ad
sheepshaker1011 <fancheyu1993@hotmail.com>
2018-06-27 00:49:16 +0800

    fix upload

--- views.py
+++ views.py
@@ -53,10 +53,7 @@ def dateAppended1(comp, df_hz):
         df_comp_today = pd.read_csv(io.StringIO(comp_today), header=None, 
                  names=['rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield'], 
                  delimiter=r"\s+", dtype={'bond_code':str})
-	print("getvalue:", io.StringIO(comp_today).getvalue())
-	print(df_comp_today)
         compdf = pd.concat([compdf,df_comp_today])
-        print(compdf)
     return compdf
     
 def dateAppended2(comp, df_hz):
@@ -158,8 +155,7 @@ def extract_excel():
         dict_compname = {0:"国际", 1:"国利", 2:"BGC", 3:"平安", 4:"信唐"}
         df3_final = pd.DataFrame(columns=['trade_date', 'rem_period', 'bond_code', 
                                     'bond_name', 'bond_rating', 'bond_yield','note'])
-        #trade_date = df1['trade_date'][0]
-	trade_date = '2018-06-21'
+        trade_date = df1['trade_date'][0]
         # uniform format
         for comp in range(0,5):
             if comp in [0]:
@@ -177,7 +173,6 @@ def extract_excel():
         df3_final = df3_final[df3_final['bond_name'].notnull()]
         df3_final = df3_final.where(df3_final.notnull(), None)
         list2Write3 = df3_final.to_dict(orient="records")
-	print(list2Write3)
         session = Session()
         session.execute(Huizong.__table__.insert(), list2Write3)
         session.commit()
@@ -189,53 +184,5 @@ def extract_excel():
         flash("文件有误 :(")
     return redirect("/")
 
-
-
-
-"""
-        # extract and transform
-        if "月信用成交汇总" in filename:
-            df = to_dfhz(path)
-            database_name = FiveComps
-        if "每日成交情况" in filename:
-            df = pd.read_excel(path, header=None, sheet_name=0)
-            df = df.iloc[:,0:10]
-            # uniform % and float
-            df.columns = ['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note', 'offer', 'bid', 'amount']
-            df['bond_yield'] = df['bond_yield'].apply(lambda x: (float(x)*100) if (isfloat(x) and float(x)<1) else x)
-            database_name = Yaosu
-        if "外部成交情况" in filename:
-            df1 = pd.read_excel(path, header=None, sheet_name=0)
-            df2 = pd.read_excel(path, header=None, sheet_name=1)
-            df = pd.concat([df1, df2])
-            df = df.iloc[:,0:9]
-            df[10] = None
-            # uniform % and float
-            df.columns = ['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note', 'offer', 'bid', 'amount']
-            database_name = WaiBu
-
-        df = df[df['bond_name'].notnull()]
-        # 处理 NaT：使用 to_datetime 将非 date 字符转换成 NaT，再用 strftime 将 NaT 转换成可以被 mysql 识别的 None
-        df['trade_date'] = pd.to_datetime(df['trade_date'], errors="coerce").apply(lambda x: x.strftime('%Y-%m-%d')if not pd.isnull(x) else None)
-        df = df.where(df.notnull(), None)
-        df = df[df['trade_date'] == request.form['upload_date']]
-        list2Write = df.to_dict(orient="records")
-        df_json = df.to_json(orient="records", force_ascii=False)
-        print(df_json)
-
-        return df_json
-
-@app.route('/upload', methods=['GET', 'POST'])
-@extract_excel
-def upload_excel():
-
-        # load
-    update_mysql(list2Write, database_name)
-    os.remove(path)
-    flash("上传成功！")   
-    flash("文件有误 :(")
-    return render_template("index.html")
-
-"""
 
 
