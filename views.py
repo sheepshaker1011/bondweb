15115cb342c683fda1d7f042e2c4f47521b10e8f
sheepshaker1011 <fancheyu1993@hotmail.com>
2018-06-27 00:27:51 +0800

    fix upload bug

--- views.py
+++ views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request, redirect, g, jsonify, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_, func, select
@@ -8,7 +11,9 @@ import pandas as pd
 from bondapp import app
 from bondapp import db
 from .models import YaosuNex, Huizong, YaosuOthers
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 Session = sessionmaker(db.engine)
@@ -41,19 +46,26 @@ def search_comp():
 def dateAppended1(comp, df_hz):
     compdf = pd.DataFrame(columns=['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note'])
     comp_today = ''
-    comp_today = df_hz[comp][0]+df_hz[comp][1]+df_hz[comp][2]
-    if isinstance(comp_today, str): 
+    for i in range(0,2):
+        if isinstance(df_hz[comp][i], float): df_hz[comp][i] = ''
+        comp_today += df_hz[comp][i]
+    if not (comp_today == ''): 
         df_comp_today = pd.read_csv(io.StringIO(comp_today), header=None, 
                  names=['rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield'], 
                  delimiter=r"\s+", dtype={'bond_code':str})
+	print("getvalue:", io.StringIO(comp_today).getvalue())
+	print(df_comp_today)
         compdf = pd.concat([compdf,df_comp_today])
+        print(compdf)
     return compdf
     
 def dateAppended2(comp, df_hz):
     compdf = pd.DataFrame(columns=['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note'])
     comp_today = ''
-    comp_today = df_hz[comp][0]+df_hz[comp][1]+df_hz[comp][2]
-    if isinstance(comp_today, str): 
+    for i in range(0,2):
+        if isinstance(df_hz[comp][i], float): df_hz[comp][i] = ''
+        comp_today += df_hz[comp][i]
+    if not (comp_today == ''):
         df_comp_today = pd.read_csv(io.StringIO(comp_today), header=None, 
                  names=['rem_period', 'bond_name', 'bond_code', 'bond_rating', 'bond_yield'], 
                  delimiter=r"\s+", dtype={'bond_code':str})
@@ -63,8 +75,10 @@ def dateAppended2(comp, df_hz):
 def dateAppended3(comp, df_hz):
     compdf = pd.DataFrame(columns=['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note'])
     comp_today = ''
-    comp_today = df_hz[comp][0]+df_hz[comp][1]+df_hz[comp][2]
-    if isinstance(comp_today, str): 
+    for i in range(0,2):
+        if isinstance(df_hz[comp][i], float): df_hz[comp][i] = ''
+        comp_today += df_hz[comp][i]
+    if not (comp_today == ''): 
         df_comp_today = pd.read_csv(io.StringIO(comp_today), header=None, 
                  names=['rem_period', 'bond_name', 'bond_code', 'bond_yield', 'bond_rating'], 
                  delimiter=r"\s+", dtype={'bond_code':str})
@@ -74,34 +88,15 @@ def dateAppended3(comp, df_hz):
 def dateAppended4(comp, df_hz):
     compdf = pd.DataFrame(columns=['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note'])
     comp_today = ''
-    comp_today = df_hz[comp][0]+df_hz[comp][1]+df_hz[comp][2]
-    if isinstance(comp_today, str): 
+    for i in range(0,2):
+        if isinstance(df_hz[comp][i], float): df_hz[comp][i] = ''
+        comp_today += df_hz[comp][i]
+    if not (comp_today == ''): 
         df_comp_today = pd.read_csv(io.StringIO(comp_today), header=None, 
                  names=['rem_period', 'bond_code', 'bond_name', 'bond_yield', 'bond_rating'], 
                  delimiter=r"\s+", dtype={'bond_code':str})
         compdf = pd.concat([compdf,df_comp_today])
     return compdf
-
-def to_dfhz(path):
-    df_hz = pd.read_excel(path, sheet_name=1, 
-                      skiprows=3, header=None, parse_cols="B:F")
-    dict_compname = {0:"国际", 1:"国利", 2:"BGC", 3:"平安", 4:"信唐"}
-    df_hz_final = pd.DataFrame(columns=['trade_date', 'rem_period', 'bond_code', 
-                                    'bond_name', 'bond_rating', 'bond_yield','note'])
-    # uniform format
-    for comp in range(0,5):
-        if comp in [0]:
-            compdf = dateAppended1(comp, df_hz)
-        if comp in [1,2]:
-            compdf = dateAppended4(comp, df_hz)
-        if comp in [3]:
-            compdf = dateAppended2(comp, df_hz)
-        if comp in [4]:
-            compdf = dateAppended3(comp, df_hz)
-        compdf['note'] = dict_compname[note] # add trading comp's name
-        df_hz_final = pd.concat([df_hz_final, compdf])
-
-    return df_hz_final
 
 def isfloat(x):
     try:
@@ -130,38 +125,41 @@ def extract_excel():
         file.save(path)
 
         df1 = pd.read_excel(path, header=None, sheet_name=0)
-        df1 = df1.iloc[:,0:10]
-        df1.columns = ['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note', 'offer', 'bid', 'amount']
-        df1['bond_yield'] = df1['bond_yield'].apply(lambda x: (float(x)*100) if (isfloat(x) and float(x)<1) else x)
-        df1 = df1[df1['bond_name'].notnull()]
-        # 处理 NaT：使用 to_datetime 将非 date 字符转换成 NaT，再用 strftime 将 NaT 转换成可以被 mysql 识别的 None
-        df1['trade_date'] = pd.to_datetime(df1['trade_date'], errors="coerce").apply(lambda x: x.strftime('%Y-%m-%d')if not pd.isnull(x) else None)
-        df1 = df1.where(df1.notnull(), None)
-        list2Write1 = df1.to_dict(orient="records")
-        session = Session()
-        session.execute(YaosuNex.__table__.insert(), list2Write1)
-        session.commit()
-        session.close()
+	if not df1.empty:
+		df1 = df1.iloc[:,0:10]
+		df1.columns = ['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note', 'offer', 'bid', 'amount']
+		df1['bond_yield'] = df1['bond_yield'].apply(lambda x: (float(x)*100) if (isfloat(x) and float(x)<1) else x)
+		df1 = df1[df1['bond_name'].notnull()]
+		# 处理 NaT：使用 to_datetime 将非 date 字符转换成 NaT，再用 strftime 将 NaT 转换成可以被 mysql 识别的 None
+		df1['trade_date'] = pd.to_datetime(df1['trade_date'], errors="coerce").apply(lambda x: x.strftime('%Y-%m-%d')if not pd.isnull(x) else None)
+		df1 = df1.where(df1.notnull(), None)
+		list2Write1 = df1.to_dict(orient="records")
+		session = Session()
+		session.execute(YaosuNex.__table__.insert(), list2Write1)
+		session.commit()
+		session.close()
 
-        df2 = pd.read_excel(path, header=None, sheet_name=1)
-        df2 = df2.iloc[:,0:10]
-        df2.columns = ['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note', 'offer', 'bid']
-        df2 = df2[df2['bond_name'].notnull()]
-        # 处理 NaT：使用 to_datetime 将非 date 字符转换成 NaT，再用 strftime 将 NaT 转换成可以被 mysql 识别的 None
-        df2['trade_date'] = pd.to_datetime(df2['trade_date'], errors="coerce").apply(lambda x: x.strftime('%Y-%m-%d')if not pd.isnull(x) else None)
-        df2['note'] = '外部'
-        df2 = df2.where(df2.notnull(), None)
-        list2Write2 = df2.to_dict(orient="records")
-        session = Session()
-        session.execute(YaosuOthers.__table__.insert(), list2Write2)
-        session.commit()
-        session.close()        
+        df2 = pd.read_excel(path, header=None, sheet_name=1)	
+	if not df2.empty:
+		df2 = df2.iloc[:,0:10]
+		df2.columns = ['trade_date', 'rem_period', 'bond_code', 'bond_name', 'bond_rating', 'bond_yield', 'note', 'offer', 'bid']
+		df2 = df2[df2['bond_name'].notnull()]
+		# 处理 NaT：使用 to_datetime 将非 date 字符转换成 NaT，再用 strftime 将 NaT 转换成可以被 mysql 识别的 None
+		df2['trade_date'] = pd.to_datetime(df2['trade_date'], errors="coerce").apply(lambda x: x.strftime('%Y-%m-%d')if not pd.isnull(x) else None)
+		df2['note'] = '外部'
+		df2 = df2.where(df2.notnull(), None)
+		list2Write2 = df2.to_dict(orient="records")
+		session = Session()
+		session.execute(YaosuOthers.__table__.insert(), list2Write2)
+		session.commit()
+		session.close()        
 
-        df3 = pd.read_excel(path, sheet_name=2, skiprows=3, header=None, parse_cols="B:F")
+        df3 = pd.read_excel(path, sheet_name=2, skiprows=3, header=None, usecols="B:F")
         dict_compname = {0:"国际", 1:"国利", 2:"BGC", 3:"平安", 4:"信唐"}
         df3_final = pd.DataFrame(columns=['trade_date', 'rem_period', 'bond_code', 
                                     'bond_name', 'bond_rating', 'bond_yield','note'])
-        trade_date = df1['trade_date'][0]
+        #trade_date = df1['trade_date'][0]
+	trade_date = '2018-06-21'
         # uniform format
         for comp in range(0,5):
             if comp in [0]:
@@ -179,6 +177,7 @@ def extract_excel():
         df3_final = df3_final[df3_final['bond_name'].notnull()]
         df3_final = df3_final.where(df3_final.notnull(), None)
         list2Write3 = df3_final.to_dict(orient="records")
+	print(list2Write3)
         session = Session()
         session.execute(Huizong.__table__.insert(), list2Write3)
         session.commit()
